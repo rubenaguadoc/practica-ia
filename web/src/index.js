0705cda4dfbfa4135755614974714eff3d3602d2
Rubén <rubenaguadoc@gmail.com>
2019-11-07 09:15:25 +0100

    Tube map skelleton

--- web/src/index.js
+++ web/src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+const d3 = Object.assign({}, require('d3'), require('d3-tube-map'));
+const data = require('./pubs.json');
+
+const container = d3.select('#tube-map');
+
+const width = 1000;
+const height = 500;
+
+const map = d3
+  .tubeMap()
+  .width(width)
+  .height(height)
+  .margin({
+    top: 20,
+    right: 20,
+    bottom: 40,
+    left: 100,
+  })
+  .on('click', name => {
+    console.log(name);
+  });
+
+container.datum(data).call(map);
